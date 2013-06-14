79eb48999cc116724bf90b2efd6572bed7112cdd
Fábio Freire da Silva <fabricio_elton@hotmail.com>
2013-06-14 14:33:49 -0300

    primeiro commit

---
+++ src/CalculadoraLib.java
@@ -0,0 +1,22 @@
+
+public class CalculadoraLib {
+
+	public double soma(double a, double b) {
+		return a + b;
+	}
+
+
+	public double subtracao(double a, double b) {
+		return a - b;
+	}
+
+
+	public double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+
+	public double divisao(double a, double b) {
+		return a / b;
+	}
+}
---
+++ src/CalculadoraTest.java
@@ -0,0 +1,32 @@
+import junit.framework.TestCase;
+
+
+public class CalculadoraTest extends TestCase {
+
+	private CalculadoraLib calculadora =  new CalculadoraLib();
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testSoma() { 
+		assertEquals(15.0, calculadora.soma(5.0, 10.0));
+	}
+	
+	public void testSubtracao() {
+		assertEquals(15.0, calculadora.subtracao(30.0, 15.0));
+	}
+	
+	public void testMultiplicacao() {
+		assertEquals(15.0, calculadora.multiplicacao(3.0, 5.0));
+	}
+	
+	public void testDiv() {
+		assertEquals(3.0, calculadora.divisao(15.0, 5.0));
+	}
+	
+}
