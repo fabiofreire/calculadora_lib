2ad4b5cb735517473bb13c12a17ea4d0a8aab3f5
Fábio Freire da Silva <fabricio_elton@hotmail.com>
2013-06-14 14:52:56 -0300

    travis file

--- src/CalculadoraLib.java
+++ src/CalculadoraLib.java
@@ -1,7 +1,12 @@
+import org.apache.log4j.Logger;
+
 
 public class CalculadoraLib {
 
+	static Logger log4j = Logger.getLogger("test");
+	
 	public double soma(double a, double b) {
+		log4j.warn("Low fuel level.");
 		return a + b;
 	}
 
